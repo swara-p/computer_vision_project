3bc649b095c2c78f3a62a0c5d1405da3bb9c040c
priya666-0ss <74360192+priya666-0ss@users.noreply.github.com>
2024-04-29 00:50:33 +0530

    Add files via upload

---
+++ RANSAC.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+import math
+import random
+
+def homography(pairs):
+    rows = []
+    # print("pairs", pairs, len(pairs))
+    # print("-------")
+    for i in range(pairs.shape[0]):
+        p1 = np.append(pairs[i][0:2], 1)
+        p2 = np.append(pairs[i][2:4], 1)
+        row1 = [0, 0, 0, p1[0], p1[1], p1[2], -p2[1]*p1[0], -p2[1]*p1[1], -p2[1]*p1[2]]
+        row2 = [p1[0], p1[1], p1[2], 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1], -p2[0]*p1[2]]
+        rows.append(row1)
+        rows.append(row2)
+    rows = np.array(rows)
+    U, s, V = np.linalg.svd(rows)
+    H = V[-1].reshape(3, 3)
+    H = H/H[2, 2] # standardize to let w*H[2,2] = 1
+    # print("H shape",H.shape)
+    return H
+
+def random_point(matches, k=4):
+    # print("matches", matches)
+    # print("------------")
+    idx = random.sample(range(len(matches)), k)
+    point = [matches[i] for i in idx ]
+    return np.array(point)
+
+def get_error(points, H):
+    # print("points", points, points.shape)
+    # print("-------")
+    num_points = len(points)
+    # print(f"points 0 2: {points[:, 0:2]}")
+    all_p1 = np.concatenate((points[:, 0:2], np.ones((num_points, 1))), axis=1)
+    all_p2 = points[:, 2:4]
+    estimate_p2 = np.zeros((num_points, 2))
+    for i in range(num_points):
+        temp = np.dot(H, all_p1[i])
+        estimate_p2[i] = (temp/temp[2])[0:2] # set index 2 to 1 and slice the index 0, 1
+    # Compute error
+    errors = np.linalg.norm(all_p2 - estimate_p2 , axis=1) ** 2
+
+    return errors
+
+def ransac(matches, kpsA, kpsB, threshold, iters):
+    if len(matches) < 4:
+        return None
+    src_pts = np.float32(
+        [kpsA[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32(
+        [kpsB[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+    matches = np.array([np.concatenate([src_pts[i], dst_pts[i]], axis=1)[0] for i in range(len(src_pts))])
+    num_best_inliers = 0
+    for i in range(iters):
+        points = random_point(matches)
+        H = homography(points)
+        
+        #  avoid dividing by zero 
+        if np.linalg.matrix_rank(H) < 3:
+            continue
+        # print("matches shape", np.array(matches))
+        errors = get_error(matches, H)
+        idx = np.where(errors < threshold)[0]
+        inliers = matches[idx]
+
+        num_inliers = len(inliers)
+        if num_inliers > num_best_inliers:
+            best_inliers = inliers.copy()
+            num_best_inliers = num_inliers
+            best_H = H.copy()
+            
+    print("inliers/matches: {}/{}".format(num_best_inliers, len(matches)))
+    return best_inliers, best_H
+
+def getHomography(kpsA, kpsB, matches, thresh):
+    if len(matches) > 4:
+        src_pts = np.float32([ kpsA[m.queryIdx].pt for m in matches]).reshape(-1,1,2)
+        dst_pts = np.float32([ kpsB[m.trainIdx].pt for m in matches]).reshape(-1,1,2)
+        # estimate the homography between the sets of points
+        (M, mask) = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, thresh)
+        mask = mask.ravel().tolist()
+        return (M, mask)
+    else:
+        return None
